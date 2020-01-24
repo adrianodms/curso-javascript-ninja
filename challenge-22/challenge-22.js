9227a8b4317eea7c676d1d33b25bdf1c37bd4355
Adriano Dutra Magalhães <adrianomagalhaes@gera.com.br>
2020-01-24 08:08:45 -0300

    23

--- challenge-22/challenge-22.js
+++ challenge-22/challenge-22.js
@@ -1,8 +1,21 @@
+(function () {
+  'use strict';
+
   /*
   Crie dois objetos, que serão duas pessoas. Cada um deve ter as propriedades
   `name` e `lastName`, preenchidos com o nome e sobrenome da pessoa.
   */
-  // ?
+  function Person(name, lastName) {
+    this.name = name;
+    this.lastName = lastName;
+  }
+
+  function getFullName() {
+    return this.name + ' ' + this.lastName;
+  }
+
+  var adriano = new Person('Adriano', 'Magalhães');
+  var elaine = new Person('Elaine', 'Leite');
 
   /*
   Agora crie uma função chamada `getFullName` que retorne as propriedades
@@ -10,13 +23,14 @@
   A função não deve receber nenhum parâmetro, mas as propriedades `name` e
   `lastName` devem ser dinâmicas.
   A mesma função deve servir para as duas pessoas (ou qualquer outra que for
-  criada).
-  Depois disso, invoque essa função, mostrando no console o nome completo das
-  pessoas que foram criadas anteriormente, passando as pessoas acima como
-  contexto da função. Use um console.log por pessoa.
-  */
-  console.log( 'O nome das pessoas é:' );
-  // ?
+   criada).
+   Depois disso, invoque essa função, mostrando no console o nome completo das
+   pessoas que foram criadas anteriormente, passando as pessoas acima como
+   contexto da função. Use um console.log por pessoa.
+   */
+  console.log('O nome das pessoas é:');
+  console.log(getFullName.call(adriano));
+  console.log(getFullName.call(elaine));
 
   /*
   Crie uma função chamada `sum`. Essa função pode receber uma lista de
@@ -25,46 +39,59 @@
   Na primeira linha, dentro da função, deixe um console.log para mostrar todos
   os parâmetros passados para essa função.
   */
-  // ?
+  function sum() {
+    console.log(arguments);
+    return Array.prototype.reduce.call(arguments,
+      function (acummulated, actual) {
+        return acummulated + actual;
+      });
+  }
 
   /*
   Mostre no console que a função acima funciona, invocando-a em 3 console.log
   diferentes, com quantidades variáveis de parâmetros passados.
   */
-  console.log( '\nSomar alguns números:' );
-  // ?
-
+  console.log('\nSomar alguns números:');
+  console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+  console.log(sum(11, 22, 33, 44, 55, 66, 77, 88, 99, 100));
+  console.log(sum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
   /*
   Declare uma variável chamada `userEntry`, que irá receber alguns valores
   entrados pelo usuário. Mostre para o usuário a seguinte frase:
   "Entre com alguns números que serão somados:"
   */
-  // ?
+  var userEntry = prompt('Entre com alguns números que serão somados:');
 
   /*
   Mostre no console o valor entrado pelo usuário:
   */
-  console.log( '\nEntrada do usuário:' );
-  // ?
+  console.log('\nEntrada do usuário:');
+  console.log(userEntry);
 
   /*
   Crie uma função chamada `justNumbers`, que recebe por parâmetro uma string
   e remove tudo o que não for número, retornando um array somente com os números
   da string. Mostre a representação em string dessa função no console.
   */
-  console.log( '\nFunção que limpa entrada do usuário (somente números):' );
-  // ?
+  console.log('\nFunção que limpa entrada do usuário (somente números):');
+  function justNumbers(str) {
+    return str.replace(/\D+/g, ',').split(',').map(function (number) { return Number(number) });
+  }
+  console.log(justNumbers.toString());
 
   /*
   Usando a função acima, faça a limpeza dos valores entrados pelo usuário,
   atribuindo o resultado à uma variável `numbers`.
   */
-  console.log( '\nEntrada do usuário limpa. Somente números:' );
-  // ?
+  console.log('\nEntrada do usuário limpa. Somente números:');
+  var numbers = justNumbers(userEntry);
+  console.log(numbers);
 
   /*
   Agora com o array de números, utilize a função `sum` para somar todos os
   números desse array e mostre o resultado no console.
   */
-  console.log( '\nSomar números entrados pelo usuário:' );
-  // ?
+  console.log('\nSomar números entrados pelo usuário:');
+  console.log(sum.apply(sum, numbers));
+
+})()
--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,119 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function (win, doc) {
+  'use strict';
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+  /*
+  Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+  As regras são:
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+  - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+  diretamente;
+  - O input deve iniciar com valor zero;
+  - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+  - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+  multiplicação(x) e divisão(÷);
+  - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+  que irá limpar o input, deixando-o com valor 0;
+
+  - A cada número pressionado, o input deve atualizar concatenando cada valor
+  digitado, como em uma calculadora real;
+  - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+  operação no input. Se o último caractere no input já for um símbolo de alguma
+  operação, esse caractere deve ser substituído pelo último pressionado.
+  Exemplo:
+  - Se o input tem os valores: "1+2+", e for pressionado o botão de
+  multiplicação (x), então no input deve aparecer "1+2x".
+  - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+  input;
+  - Ao pressionar o botão "CE", o input deve ficar zerado.
+  */
+
+  var $display = doc.querySelector('[data-js="display"]');
+  var $numberButtons = doc.querySelectorAll('[data-js="number"]');
+  var $operationButtons = doc.querySelectorAll('[data-js="operation"]');
+  var $equalButton = doc.querySelector('[data-js="equal"]');
+  var $clearButton = doc.querySelector('[data-js="clear"]');
+  initButtons();
+
+  function Calculator() {
+    function sum(a, b) {
+      return a + b;
+    };
+    function substraction(a, b) {
+      return a - b;
+    };
+    function division(a, b) {
+      return a / b;
+    };
+    function multiplication(a, b) {
+      return a * b;
+    };
+    function operation(operator) {
+      switch (operator) {
+        case '+':
+          return sum;
+        case '-':
+          return substraction;
+        case 'x':
+        case '*':
+          return multiplication;
+        case '/':
+        case '÷':
+          return division;
+        default:
+          throw new Error('Operação inexistente');
+      }
+    }
+    this.executeOperation = function (strOperation) {
+      var priorityOperationsRegex = /(\d+[*x\/÷]\d+){1,}/g;
+      var operationsPartsRegex = /^(\d+)([\+\-\*x\/÷])(\d+)$/g;
+
+      // var priorityOperations = priorityOperationsRegex.exec(strOperation);
+      // var result = 0;
+      // while (priorityOperations.length) {
+      //   priorityOperations.forEach(function (item) {
+      //     var operationItems = item.match(operationsPartsRegex);
+      //     var operation = this.operation(operationItems[1])
+      //     result += operation.apply(operation, [operationItems[0], operationItems[2]]);
+      //   });
+      //   priorityOperations.pop();
+      // }
+
+      var operations = strOperation.match(/(\d+[\+\-\*x\/÷]\d+)/g);
+    }
+  }
+
+
+  function initButtons() {
+    $numberButtons.forEach(function ($button) {
+      $button.addEventListener('click', function () {
+        if ($display.value == '0') {
+          $display.value = '';
+        }
+        $display.value += $button.value;
+      });
+    });
+
+    $operationButtons.forEach(function ($button) {
+      $button.addEventListener('click', function () {
+        var value = $display.value;
+        var operationsInTheEndRegex = /(.+)[\+\-\*x\/÷]$/;
+        if (value.match(operationsInTheEndRegex)) {
+          value = value.replace(operationsInTheEndRegex, '$1' + $button.value);
+        } else {
+          value += $button.value;
+        }
+        $display.value = value;
+      });
+    });
+
+    $clearButton.addEventListener('click', function () {
+      $display.value = 0;
+    });
+
+
+    $equalButton.addEventListener('click', function () {
+      var calculator = new Calculator();
+      calculator.executeOperation($display.value);
+    });
+  }
+})(window, document);
