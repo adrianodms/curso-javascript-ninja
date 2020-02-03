dabcaffa8a18d75ce8548cc4eda722c57daf09c4
Adriano Dutra Magalhães <adrianomagalhaes@gera.com.br>
2020-02-03 08:12:06 -0300

    28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,4 +1,4 @@
-  (function(DOM){
+(function (DOM) {
 
   /*
   No HTML:
@@ -9,7 +9,7 @@
   preenchidas com os dados da requisição feita no JS.
   - Crie uma área que receberá mensagens com o status da requisição:
   "Carregando, sucesso ou erro."
-  
+
   No JS:
   - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
   deve ser limpo e enviado somente os números para a requisição abaixo;
@@ -28,6 +28,54 @@
   adicionar as informações em tela.
   */
 
-console.log(DOM);
- 
+  var CEPinput = (new DOM('[data-js=input-cep]')).get()[0];
+  var submitBtn = new DOM('[data-js="buscar"]');
+
+  var logradouroSpan = (new DOM('[data-js=logradouro]')).get()[0];
+  var bairroSpan = (new DOM('[data-js=bairro]')).get()[0];
+  var estadoSpan = (new DOM('[data-js=estado]')).get()[0];
+  var cidadeSpan = (new DOM('[data-js=cidade]')).get()[0];
+  var cepSpan = (new DOM('[data-js=cep]')).get()[0];
+
+  var ajaxStatus = (new DOM('[data-js=ajax-status]')).get()[0];
+
+  submitBtn.on('click', function () {
+    fillAddress('', '', '', '', '');
+
+    var cep = CEPinput.value.replace(/\D/g, '');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://viacep.com.br/ws/' + cep + '/json/');
+    xhr.send();
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState == 1 || this.readyState == 2 || this.readyState == 3) {
+        ajaxStatus.innerText = 'Buscando informações para o CEP ' + cep + '...';
+      }
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          var response = JSON.parse(xhr.responseText);
+
+          fillAddress(response.logradouro, response.bairro, response.uf, response.localidade, response.cep);
+
+          ajaxStatus.innerText = 'Endereço referente ao CEP: ' + cep;
+
+        } else {
+          ajaxStatus.innerText = 'Não encontramos o endereço para o CEP ' + cep + '.';
+        }
+
+      }
+    });
+
+  });
+
+  function fillAddress(logradouro, bairro, uf, localidade, cep) {
+    logradouroSpan.innerText = logradouro;
+    bairroSpan.innerText = bairro;
+    estadoSpan.innerText = uf;
+    cidadeSpan.innerText = localidade;
+    cepSpan.innerText = cep;
+  }
+
 })(window.DOM);
+
+
+
